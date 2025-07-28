41d75c3373b24a2be34fbc493139b3da61412664
kukiwakamechan <165251802+kukiwakamechan@users.noreply.github.com>
2025-07-28 13:21:30 +0900

    Update log.sh

--- package/tcpdump/log.sh
+++ package/tcpdump/log.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # ログ保存のディレクトリ作成 
-[ ! -d "/home/kaho/develop/AuthSpeedTracker/package/tcpdump/log/sh_log" ] && mkdir -p "/home/kaho/develop/AuthSpeedTracker/package/tcpdump/log/sh_log"
+[ ! -d "パス" ] && mkdir -p "パス"
 
 # 日付と時刻の取得
 current_datetime=$(date +"%Y-%m-%d_%H-%M-%S")
 
 # SSHのアクセスログを取得
-sudo tcpdump -i enp1s0 -s 0 -U -w "/home/kaho/develop/AuthSpeedTracker/package/tcpdump/log/sh_log/${current_datetime}.dump" 'tcp port 22'
+sudo tcpdump -i enp1s0 -s 0 -U -w "パス/${current_datetime}.dump" 'tcp port 22'
 
 
 
